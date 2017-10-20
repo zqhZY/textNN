11a6c384091159430e653e6057e1e981dfff4b6d
zqhZY <zqingheng@163.com>
2017-10-20 08:58:41 +0800

    change logic runner.py

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
 import sys
 import torch
+import os
 import torch.autograd as autograd
 import torch.nn.functional as F
 
@@ -8,21 +9,23 @@ def fit(train, val, model, args):
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
-    steps = 0
-    model.train()
     if args.cuda:
         model.cuda()
+    steps = 0
+    model.train()
+    best_accuracy = 0
     for epoch in range(1, args.epochs+1):
         for batch in train:
-
+            #print("batch")
             feature, target = batch.text, batch.labels
             # print target
 
+            feature.data.t_()
             target.data.sub_(1)
-
-            x = feature.data.numpy()
-            x = x.T
-            feature = autograd.Variable(torch.from_numpy(x))
+            #print(target.data)
+            #x = feature.data.numpy()
+            #x = x.T
+            #feature = autograd.Variable(torch.from_numpy(x))
             # print feature[0]
             # print type(feature)
             if args.cuda:
@@ -32,9 +35,9 @@ def fit(train, val, model, args):
             out = model(feature)
             # print out
 
-            # print out
-            # print target.view(-1)
-            loss = F.cross_entropy(out, target.view(-1))
+            #print(out)
+            #print(target.view(-1))
+            loss = F.cross_entropy(out, target)
             loss.backward()
             optimizer.step()
 
@@ -49,8 +52,16 @@ def fit(train, val, model, args):
                                                                              accuracy,
                                                                              corrects,
                                                                              batch.batch_size))
-            if steps % 100 == 0:
-                eval(val, model, args)
+            if steps % 10 == 0:
+                current_accuracy = eval(val, model, args)
+                if current_accuracy > best_accuracy:
+                    best_accuracy = current_accuracy
+                    if not os.path.isdir(args.save_dir):
+                        os.makedirs(args.save_dir)
+                    print("save model at step %s, accuracy is %d".format(str(steps), current_accuracy))
+                    save_path = os.path.join(args.save_dir, "snapshot.mdl")
+                    torch.save(model, save_path)
+                #eval(val, model, args)
 
 
 def eval(data_iter, model, args):
@@ -59,10 +70,10 @@ def eval(data_iter, model, args):
     for batch in data_iter:
         feature, target = batch.text, batch.labels
         target.data.sub_(1)  # batch first, index align
-
-        x = feature.data.numpy()
-        x = x.T
-        feature = autograd.Variable(torch.from_numpy(x))
+        feature.data.t_()
+        #x = feature.data.numpy()
+        #x = x.T
+        #feature = autograd.Variable(torch.from_numpy(x))
 
         if args.cuda:
             feature, target = feature.cuda(), target.cuda()
@@ -82,3 +93,4 @@ def eval(data_iter, model, args):
                                                                        accuracy,
                                                                        corrects,
                                                                         size))
+    return accuracy
