0b7bcc3395f6b484748b42b9e61b6e64c11da35f
zqhZY <zqingheng@163.com>
2017-10-24 15:42:06 +0800

    add dropout and avg_popling

--- models/TextCNNBN.py
+++ models/TextCNNBN.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 
 class TextCNN(nn.Module):
@@ -15,7 +16,7 @@ class TextCNN(nn.Module):
         Cin = 1
         Cout = args.kernel_num
         Ks = args.kernel_sizes
-        args.linear_hidden_size = 512
+        args.linear_hidden_size = 1024
 
         self.embeding = nn.Embedding(V, D)
         # self.convs = nn.ModuleList([nn.Conv2d(Cin, Cout, (K, D)) for K in Ks])
@@ -24,6 +25,8 @@ class TextCNN(nn.Module):
                                         out_channels = Cout,
                                         kernel_size = (K, D)),
                                 nn.BatchNorm2d(Cout),
+                                nn.ReLU(inplace=True),
+                                nn.Dropout(0.2),
                                 # nn.ReLU(inplace=True),
                                 # nn.MaxPool1d(kernel_size = (opt.title_seq_len - kernel_size + 1))
                             )
@@ -33,16 +36,20 @@ class TextCNN(nn.Module):
             nn.Linear(len(Ks)*Cout, args.linear_hidden_size),
             nn.BatchNorm1d(args.linear_hidden_size),
             nn.ReLU(inplace=True),
+            nn.Dropout(args.dropout),
             nn.Linear(args.linear_hidden_size, C)
         )
 
+        if args.embedding_path:
+            self.embeding.weight.data.copy_(torch.from_numpy(np.load(args.embedding_path)))
 
-        # self.dropout = nn.Dropout(args.dropout)
-        # self.fc = nn.Linear(len(Ks)*Cout, C)
+        #self.dropout = nn.Dropout(args.dropout)
+        #self.fc = nn.Linear(len(Ks)*Cout, C)
 
     def conv_and_pool(self, x, conv):
-        x = F.relu(conv(x)).squeeze(3)  # (N,Co,W)
-        x = F.max_pool1d(x, x.size(2)).squeeze(2)
+        #x = F.relu(conv(x)).squeeze(3)  # (N,Co,W)
+        x = F.x.squeeze(3)  # (N,Co,W)
+        x = F.avg_pool1d(x, x.size(2)).squeeze(2)
         return x
 
     def forward(self, x):
@@ -59,7 +66,7 @@ class TextCNN(nn.Module):
                 x3 = self.conv_and_pool(x,self.conv15) #(N,Co)
                 x = torch.cat((x1, x2, x3), 1) # (N,len(Ks)*Co)
         '''
-        # x = self.dropout(x)
+        #x = self.dropout(x)
         out = self.fc(x)
         return out
 
